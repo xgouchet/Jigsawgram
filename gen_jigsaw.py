17613e0ebd0b61abaa2259c9331fcf26203ee454
Xavier Gouchet <xavier.gouchet@gmail.com>
2021-08-31 12:52:07 +0200

    Generate stories

--- gen_jigsaw.py
+++ gen_jigsaw.py
@@ -5,6 +5,7 @@
 import os
 import sys
 import json
+import numpy
 from argparse import ArgumentParser, Namespace
 from typing import List
 
@@ -13,16 +14,26 @@ from PIL import Image
 TEMPLATES = ["a", "b", "c", "d", "e", "f", "g"]
 ROW_COUNT = 3
 
-TARGET_SIZE = 1024
-MARGIN_SIZE = 72
+INPUT_WIDTH = 1168
+INPUT_HEIGHT = 1168
 
-INPUT_SIZE = TARGET_SIZE + (2 * MARGIN_SIZE)
+TARGET_WIDTH_POST = 1024
+TARGET_HEIGHT_POST = 1024
 
+
+RAW_WIDTH_STORY = 2880
+RAW_HEIGHT_STORY = 2880
+
+TARGET_WIDTH_STORY = 1080
+TARGET_HEIGHT_STORY = 1920
+
+OVERLAP = 72
+DOUBLE_OVERLAP = OVERLAP * 2
 
 def parse_arguments(args: list) -> Namespace:
     parser = ArgumentParser()
-    subparsers = parser.add_subparsers(help='Generation mode')
 
+    subparsers = parser.add_subparsers(help='Generation mode')
     parser_single = subparsers.add_parser("single")
     parser_single.add_argument("-i", "--index", required=True, type=int, help="the index of the tile in Instagram")
     parser_single.add_argument("-m", "--missing", nargs='*', type=int, required=False,
@@ -40,7 +51,16 @@ def get_source_image(index: int) -> Image:
         return Image.open(fp, mode='r')
     else:
         print(" ⚠ File " + fp + " does not exist")
-        return Image.new("RGB", (INPUT_SIZE, INPUT_SIZE), (0, 0, 0))
+        return Image.new("RGB", (INPUT_WIDTH, INPUT_HEIGHT), (0, 0, 0))
+
+
+def get_post_image(index: int) -> Image:
+    fp = "output/raw_" + str(index) + ".png"
+    if os.path.isfile(fp):
+        return Image.open(fp, mode='r')
+    else:
+        print(" ⚠ File " + fp + " does not exist")
+        return Image.new("RGB", (TARGET_WIDTH_POST, TARGET_HEIGHT_POST), (0, 0, 0))
 
 
 def get_overlay_image(index: int) -> Image:
@@ -49,7 +69,7 @@ def get_overlay_image(index: int) -> Image:
         return Image.open(fp)
     else:
         print(" ⚠ File " + fp + " does not exist")
-        return Image.new("RGBA", (INPUT_SIZE, INPUT_SIZE), (0, 0, 0, 0))
+        return Image.new("RGBA", (INPUT_WIDTH, INPUT_HEIGHT), (0, 0, 0, 0))
 
 
 def get_mask_image(index: int) -> Image:
@@ -59,7 +79,7 @@ def get_mask_image(index: int) -> Image:
         return Image.open(fp, mode='r')
     else:
         print(" ⚠ File " + fp + " does not exist")
-        return Image.new("RGB", (INPUT_SIZE, INPUT_SIZE), (0, 0, 0))
+        return Image.new("RGB", (INPUT_WIDTH, INPUT_HEIGHT), (0, 0, 0))
 
 
 def get_bump_image(index: int) -> Image:
@@ -69,7 +89,7 @@ def get_bump_image(index: int) -> Image:
         return Image.open(fp, mode='r')
     else:
         print(" ⚠ File " + fp + " does not exist")
-        return Image.new("RGB", (INPUT_SIZE, INPUT_SIZE), (128, 128, 128))
+        return Image.new("RGB", (INPUT_WIDTH, INPUT_HEIGHT), (128, 128, 128))
 
 
 def get_stroke_image(index: int) -> Image:
@@ -79,7 +99,7 @@ def get_stroke_image(index: int) -> Image:
         return Image.open(fp, mode='r')
     else:
         print(" ⚠ File " + fp + " does not exist")
-        return Image.new("RGB", (INPUT_SIZE, INPUT_SIZE), (255, 255, 255))
+        return Image.new("RGB", (INPUT_WIDTH, INPUT_HEIGHT), (255, 255, 255))
 
 
 def get_tileid_image(index: int) -> Image:
@@ -89,21 +109,27 @@ def get_tileid_image(index: int) -> Image:
         return Image.open(fp)
     else:
         print(" ⚠ File " + fp + " does not exist")
-        return Image.new("RGB", (INPUT_SIZE, INPUT_SIZE), (255, 255, 255))
+        return Image.new("RGB", (INPUT_WIDTH, INPUT_HEIGHT), (255, 255, 255))
 
 
 def in_coords(x: int, y: int) -> int:
-    return x + (y * INPUT_SIZE)
+    return x + (y * INPUT_WIDTH)
 
 
-def generate_jigsaw(index: int, missing: List[int]):
+def generate_jigsaw(index: int, missing: List[int], with_overlay:bool, prefix: str):
     print("Generating jigsaw picture for index " + str(index) + " with missing tiles " + repr(missing))
+
+    target_width = TARGET_WIDTH_POST
+    target_height = TARGET_HEIGHT_POST
+    offset_x_in = int((INPUT_WIDTH - target_width) / 2)
+    offset_y_in = int((INPUT_HEIGHT - target_height) / 2)
     source = get_source_image(index).getdata()
     mask = get_mask_image(index).getdata()
     bump = get_bump_image(index).getdata()
     stroke = get_stroke_image(index).getdata()
     tileid = get_tileid_image(index).getdata()
-    overlay = get_overlay_image(index).getdata()
+    if with_overlay:
+        overlay = get_overlay_image(index).getdata()
 
     l_source = get_source_image(index + 1).getdata()
     l_mask = get_mask_image(index + 1).getdata()
@@ -114,23 +140,25 @@ def generate_jigsaw(index: int, missing: List[int]):
     b_source = get_source_image(index - ROW_COUNT).getdata()
     b_mask = get_mask_image(index - ROW_COUNT).getdata()
 
-    output = Image.new('RGB', (TARGET_SIZE, TARGET_SIZE), (0, 0, 0))
+    output = Image.new('RGB', (target_width, target_height), (0, 0, 0))
     output_data = output.load()
 
-    for x in range(0, TARGET_SIZE):
-        for y in range(0, TARGET_SIZE):
-            x_in = x + MARGIN_SIZE
-            y_in = y + MARGIN_SIZE
-            source_px = source[in_coords(x_in, y_in)]
-            overlay_px = overlay[in_coords(x_in, y_in)]
-            tileid_px = tileid[in_coords(x_in, y_in)]
-            tile_id = int(tileid_px[0] * 16 / 85) + \
-                      int(tileid_px[1] * 4 / 85) + \
-                      int(tileid_px[2] / 85)
-            mask_a = mask[in_coords(x_in, y_in)][0] / 256
-            bump_a = bump[in_coords(x_in, y_in)][0] / 256
-            stroke_a = stroke[in_coords(x_in, y_in)][0] / 256
-
+    for x in range(0, target_width):
+        for y in range(0, target_height):
+            x_in = x + offset_x_in
+            y_in = y + offset_y_in
+            if x_in in range(0, INPUT_WIDTH) and y_in in range(0, INPUT_HEIGHT):
+                source_px = source[in_coords(x_in, y_in)]
+                if with_overlay:
+                    overlay_px = overlay[in_coords(x_in, y_in)]
+                tileid_px = tileid[in_coords(x_in, y_in)]
+                mask_a = mask[in_coords(x_in, y_in)][0] / 256
+                bump_a = bump[in_coords(x_in, y_in)][0] / 256
+                stroke_a = stroke[in_coords(x_in, y_in)][0] / 256
+                tile_id = int(tileid_px[0] * 16 / 85) + \
+                          int(tileid_px[1] * 4 / 85) + \
+                          int(tileid_px[2] / 85)
+            
             if missing is not None and tile_id in missing:
                 pixel = (0, 0, 0)
             else:
@@ -141,36 +169,40 @@ def generate_jigsaw(index: int, missing: List[int]):
                 base[2] = (source_px[2] * mask_a)
 
                 # Left neighbour tiles
-                if x < MARGIN_SIZE:
-                    l_source_px = l_source[in_coords(x_in + TARGET_SIZE, y_in)]
-                    l_mask_a = l_mask[in_coords(x_in + TARGET_SIZE, y_in)][0] / 256
-                    base[0] = base[0] + (l_source_px[0] * l_mask_a)
-                    base[1] = base[1] + (l_source_px[1] * l_mask_a)
-                    base[2] = base[2] + (l_source_px[2] * l_mask_a)
+                if x < DOUBLE_OVERLAP - offset_x_in:
+                    if y_in in range(0, INPUT_HEIGHT):
+                        l_source_px = l_source[in_coords(x_in + INPUT_WIDTH - DOUBLE_OVERLAP, y_in)]
+                        l_mask_a = l_mask[in_coords(x_in + INPUT_WIDTH - DOUBLE_OVERLAP, y_in)][0] / 256
+                        base[0] = base[0] + (l_source_px[0] * l_mask_a)
+                        base[1] = base[1] + (l_source_px[1] * l_mask_a)
+                        base[2] = base[2] + (l_source_px[2] * l_mask_a)
 
                 # Right neighbour tiles
-                if x > TARGET_SIZE - MARGIN_SIZE:
-                    r_source_px = r_source[in_coords(x_in - TARGET_SIZE, y_in)]
-                    r_mask_a = r_mask[in_coords(x_in - TARGET_SIZE, y_in)][0] / 256
-                    base[0] = base[0] + (r_source_px[0] * r_mask_a)
-                    base[1] = base[1] + (r_source_px[1] * r_mask_a)
-                    base[2] = base[2] + (r_source_px[2] * r_mask_a)
+                if x > target_width + offset_x_in - DOUBLE_OVERLAP:
+                    if y_in in range(0, INPUT_HEIGHT):
+                        r_source_px = r_source[in_coords(x_in - INPUT_WIDTH + DOUBLE_OVERLAP, y_in)]
+                        r_mask_a = r_mask[in_coords(x_in - INPUT_WIDTH + DOUBLE_OVERLAP, y_in)][0] / 256
+                        base[0] = base[0] + (r_source_px[0] * r_mask_a)
+                        base[1] = base[1] + (r_source_px[1] * r_mask_a)
+                        base[2] = base[2] + (r_source_px[2] * r_mask_a)
 
                 # Top neighbour tiles
-                if y < MARGIN_SIZE:
-                    t_source_px = t_source[in_coords(x_in, y_in + TARGET_SIZE)]
-                    t_mask_a = t_mask[in_coords(x_in, y_in + TARGET_SIZE)][0] / 256
-                    base[0] = base[0] + (t_source_px[0] * t_mask_a)
-                    base[1] = base[1] + (t_source_px[1] * t_mask_a)
-                    base[2] = base[2] + (t_source_px[2] * t_mask_a)
+                if y < DOUBLE_OVERLAP - offset_y_in:
+                    if x_in in range(0, INPUT_WIDTH):
+                        t_source_px = t_source[in_coords(x_in, y_in + INPUT_WIDTH - DOUBLE_OVERLAP)]
+                        t_mask_a = t_mask[in_coords(x_in, y_in + INPUT_WIDTH - DOUBLE_OVERLAP)][0] / 256
+                        base[0] = base[0] + (t_source_px[0] * t_mask_a)
+                        base[1] = base[1] + (t_source_px[1] * t_mask_a)
+                        base[2] = base[2] + (t_source_px[2] * t_mask_a)
 
                 # Bottom neighbour tiles
-                if y > TARGET_SIZE - MARGIN_SIZE:
-                    b_source_px = b_source[in_coords(x_in, y_in - TARGET_SIZE)]
-                    b_mask_a = b_mask[in_coords(x_in, y_in - TARGET_SIZE)][0] / 256
-                    base[0] = base[0] + (b_source_px[0] * b_mask_a)
-                    base[1] = base[1] + (b_source_px[1] * b_mask_a)
-                    base[2] = base[2] + (b_source_px[2] * b_mask_a)
+                if y > target_height + offset_y_in - DOUBLE_OVERLAP:
+                    if x_in in range(0, INPUT_WIDTH):
+                        b_source_px = b_source[in_coords(x_in, y_in - INPUT_WIDTH + DOUBLE_OVERLAP)]
+                        b_mask_a = b_mask[in_coords(x_in, y_in - INPUT_WIDTH + DOUBLE_OVERLAP)][0] / 256
+                        base[0] = base[0] + (b_source_px[0] * b_mask_a)
+                        base[1] = base[1] + (b_source_px[1] * b_mask_a)
+                        base[2] = base[2] + (b_source_px[2] * b_mask_a)
 
                 if bump_a > 0.5:
                     factor = 1 - (2 * (bump_a - 0.5))
@@ -191,19 +223,87 @@ def generate_jigsaw(index: int, missing: List[int]):
                 int((pixel[1] * 0.1) + (pixel[1] * 0.9 * stroke_a)),
                 int((pixel[2] * 0.1) + (pixel[2] * 0.9 * stroke_a))
             )
-            overlay_src = overlay_px[3] / 256
-            overlay_dst = 1 - overlay_src
-            output_data[x, y] = (
-                int((jigsaw[0] * overlay_dst) + (overlay_px[0] * overlay_src)),
-                int((jigsaw[1] * overlay_dst) + (overlay_px[1] * overlay_src)),
-                int((jigsaw[2] * overlay_dst) + (overlay_px[2] * overlay_src))
-            )
+            if with_overlay:
+                overlay_src = overlay_px[3] / 256
+                overlay_dst = 1 - overlay_src
+                output_data[x, y] = (
+                    int((jigsaw[0] * overlay_dst) + (overlay_px[0] * overlay_src)),
+                    int((jigsaw[1] * overlay_dst) + (overlay_px[1] * overlay_src)),
+                    int((jigsaw[2] * overlay_dst) + (overlay_px[2] * overlay_src))
+                )
+            else:
+                output_data[x, y] = (jigsaw[0], jigsaw[1], jigsaw[2])
 
     if not os.path.exists('output'):
         os.makedirs('output')
-    output_path = "output/" + str(index) + ".png"
+    output_path = "output/" + prefix + "_" + str(index) + ".png"
     output.save(output_path)
     print(" ✔ Generated file " + output_path)
+
+
+def generate_story(index: int):
+    target_width = RAW_WIDTH_STORY
+    target_height = RAW_HEIGHT_STORY
+    offset_x_in = int((TARGET_WIDTH_POST - target_width) / 2)
+    offset_y_in = int((TARGET_HEIGHT_POST - target_height) / 2)
+
+    output = Image.new('RGB', (target_width, target_height), (0, 0, 0))
+    output_data = output.load()
+
+    post_tl = get_post_image(index + ROW_COUNT + 1).getdata()
+    post_t = get_post_image(index + ROW_COUNT).getdata()
+    post_tr = get_post_image(index + ROW_COUNT - 1).getdata()
+    post_l = get_post_image(index + 1).getdata()
+    post_c = get_post_image(index).getdata()
+    post_r = get_post_image(index - 1).getdata()
+    post_bl = get_post_image(index - ROW_COUNT + 1).getdata()
+    post_b = get_post_image(index - ROW_COUNT).getdata()
+    post_br = get_post_image(index - ROW_COUNT - 1).getdata()
+            
+    for x in range(0, target_width):
+        for y in range(0, target_height):
+            x_in = x + offset_x_in
+            y_in = y + offset_y_in
+
+            if x_in<0 and y_in<0:
+                post_px = post_tl[x_in + TARGET_WIDTH_POST + ((y_in+TARGET_HEIGHT_POST) * TARGET_WIDTH_POST)]
+            elif x_in<0 and y_in in range(0, TARGET_HEIGHT_POST):
+                post_px = post_l[x_in + TARGET_WIDTH_POST + (y_in * TARGET_WIDTH_POST)]
+            elif x_in<0 and y_in>=TARGET_HEIGHT_POST:
+                post_px = post_bl[x_in + TARGET_WIDTH_POST + ((y_in-TARGET_HEIGHT_POST) * TARGET_WIDTH_POST)]
+            elif x_in in range(0, TARGET_WIDTH_POST) and y_in<0:
+                post_px = post_t[x_in + ((y_in+TARGET_HEIGHT_POST) * TARGET_WIDTH_POST)]
+            elif x_in in range(0, TARGET_WIDTH_POST) and y_in in range(0, TARGET_HEIGHT_POST):
+                post_px = post_c[x_in + (y_in * TARGET_WIDTH_POST)]
+            elif x_in in range(0, TARGET_WIDTH_POST) and y_in>=TARGET_HEIGHT_POST:
+                post_px = post_b[x_in + ((y_in-TARGET_HEIGHT_POST) * TARGET_WIDTH_POST)]
+            elif x_in>=TARGET_WIDTH_POST and y_in<0:
+                post_px = post_tr[x_in - TARGET_WIDTH_POST + ((y_in+TARGET_HEIGHT_POST) * TARGET_WIDTH_POST)]
+            elif x_in>=TARGET_WIDTH_POST and y_in in range(0, TARGET_HEIGHT_POST):
+                post_px = post_r[x_in - TARGET_WIDTH_POST + (y_in * TARGET_WIDTH_POST)]
+            elif x_in>=TARGET_WIDTH_POST and y_in>=TARGET_HEIGHT_POST:
+                post_px = post_br[x_in - TARGET_WIDTH_POST + ((y_in-TARGET_HEIGHT_POST) * TARGET_WIDTH_POST)]
+            else:
+                post_px = [0, 0, 0]
+
+            output_data[x, y] = (post_px[0], post_px[1], post_px[2])
+
+    angle = ((index * 137.50309) % 42.0) - 23
+
+    output_r = output.rotate(angle, Image.BICUBIC, True)
+    dw = int((output_r.width - TARGET_WIDTH_STORY) / 2)
+    dh = int((output_r.height - TARGET_HEIGHT_STORY)/2)
+    box = (dw, dh, dw+TARGET_WIDTH_STORY, dh+TARGET_HEIGHT_STORY)
+    output_c = output_r.crop(box)
+
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    output_path = "output/story_" + str(index) + ".png"
+    output_c.save(output_path)
+    output.close()
+    output_r.close()
+    print(" ✔ Generated file " + output_path)
+
 
 def generate_list(path: str):
     with open(path) as json_file:
@@ -212,7 +312,12 @@ def generate_list(path: str):
     for key in config:
         index = int(key)
         missing = config[key]
-        generate_jigsaw(index, missing)
+        generate_jigsaw(index, missing, True, "post")
+        generate_jigsaw(index, missing, False, "raw")
+
+    for key in config:
+        index = int(key)
+        generate_story(index)
 
 
 def run_main() -> int:
@@ -222,6 +327,7 @@ def run_main() -> int:
         generate_list(cli_args.missing_list)
     else:
         generate_jigsaw(cli_args.index, cli_args.missing)
+        generate_story(cli_args.index)
 
     return 0
 
